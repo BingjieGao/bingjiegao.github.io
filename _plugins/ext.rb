6c335e18591f998b8610715f6919b7299cc8fc73
Bingjie Gao <gilman.david04@gmail.com>
2016-02-28 14:16:27 +0000

    _config_yml

--- _plugins/ext.rb
+++ _plugins/ext.rb
@@ -134,9 +134,9 @@ module Jekyll
     def tag_url(tag, type = :page, site = Tagger.site)
     	tag = tag.tr('-','')
       # tag = tag.downcase
-      print tag
+      #print tag
       url = File.join('', site.config["baseurl"].to_s, site.config["tag_#{type}_dir"],tag.downcase)
-      print url
+      #print url
       site.permalink_style == :pretty || site.config['tag_permalink_style'] == 'pretty' ? url << '/' : url << '.html'
     end
 
@@ -144,7 +144,7 @@ module Jekyll
       tags = obj['tags'].dup
       tags.map! { |t| t.first } if tags.first.is_a?(Array)
       tags.map! { |t| tag_link(t, tag_url(t), :rel => 'tag') if t.is_a?(String) }.compact!
-      print tags
+      #print tags
       tags.join(', ')
     end
 

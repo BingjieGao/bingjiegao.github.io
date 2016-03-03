69329602cb46f7e1f0e2ea7c86f97424aebef357
Bingjie Gao <gilman.david04@gmail.com>
2016-03-03 11:50:09 +0000

    dragable

--- pages/demo/javascript/skillchart-demo.js
+++ pages/demo/javascript/skillchart-demo.js
@@ -5,7 +5,7 @@
 var skillJson = [
     {
         "skill": "PHP",
-        "name":"PHP",
+        "name": "PHP",
         "profency": 3
     },
     {
@@ -39,7 +39,7 @@ var skillJson = [
         "profency": 10
     }
 ]
-var text_value='';
+var text_value = '';
 var this_bar;
 var this_value;
 var num;
@@ -52,32 +52,41 @@ var xscale = d3.scale.linear()
 var barHeight = 30;
 
 var dragable = d3.behavior.drag()
-    .on('dragstart',function(d){
+    .on('dragstart', function (d) {
         console.log(text_value);
         text_value = d.name;
-        this_bar = d3.select('rect[name='+text_value+']');
-        this_value = d3.select('.value-text[value='+text_value+']');
+        this_bar = d3.select('rect[name=' + text_value + ']');
+        this_value = d3.select('.value-text[value=' + text_value + ']');
         console.log(this_bar);
     })
-    .on('drag', function(d) {
+    .on('drag', function (d) {
         var x = d3.event.x;
-        num = (x-0.25*width)/(width*0.055);
+        num = (x - 0.25 * width) / (width * 0.055);
         num = Math.round(num);
-        d3.select(this).attr("x",num * width*0.055+0.25*width);
-        this_value.text(function(d){
-            return num+'/10';
+        d3.select(this).attr("x", function () {
+            if (x < width * 0.26) {
+                return 0.25 * width;
+            }
+            else if (x > width * 0.80) {
+                return 0.79 * width;
+            }
+            else
+                return num * width * 0.055 + 0.25 * width
+        });
+        this_value.text(function (d) {
+            return num + '/10';
         })
-        this_bar.attr('width',function(){
-            if(x<width*0.26)
-            return 0;
-            else if(x>width*0.80)
-            return width*0.55;
-            else{
-                return num * width*0.055;
+        this_bar.attr('width', function () {
+            if (x < width * 0.26)
+                return 0;
+            else if (x > width * 0.80)
+                return width * 0.55;
+            else {
+                return num * width * 0.055;
             }
         })
     })
-    .on('dragend',function(d,i){
+    .on('dragend', function (d, i) {
         skillJson[i].profency = num;
         console.log(skillJson[i]);
     })
@@ -96,8 +105,8 @@ var bars = barchart.selectAll('.bar')
     .data(skillJson)
     .enter()
     .append("rect")
-    .attr('class','bar')
-    .attr('name',function(d){
+    .attr('class', 'bar')
+    .attr('name', function (d) {
         return d.name;
     })
 
@@ -124,19 +133,19 @@ var drag = barchart.selectAll('.drag')
     .data(skillJson)
     .enter()
     .append("rect")
-    .attr('class','drag')
+    .attr('class', 'drag')
     .style("width", function (d) {
         return 10 + 'px';
     })
     .attr("transform", function (d, i) {
         return "translate(0," + i * barHeight + ")";
     })
-    .attr('x', function(d){
-        return xscale(d.profency)+0.25*width-10+'px';
+    .attr('x', function (d) {
+        return xscale(d.profency) + 0.25 * width - 10 + 'px';
     })
     .style("height", 25 + "px")
-    .style('rx','5')
-    .style('ry','5')
+    .style('rx', '5')
+    .style('ry', '5')
     .style("fill", "#6c6c6c")
     .call(dragable)
 
@@ -159,36 +168,36 @@ var text = g.append('text')
         return "translate(0," + i * barHeight + ")";
     })
     .attr('y', barHeight / 1.5)
-    .style('font-size','1em')
-    .style('fill','#6c6c6c')
+    .style('font-size', '1em')
+    .style('fill', '#6c6c6c')
 
 var value = g.append('text')
     .text(function (d) {
         return d.profency + '/10';
     })
-    .attr('value',function(d){
+    .attr('value', function (d) {
         return d.name;
     })
-    .attr('class','value-text')
+    .attr('class', 'value-text')
     .attr('y', barHeight / 1.5)
     .attr('x', '87%')
     .attr("transform", function (d, i) {
         return "translate(0," + i * barHeight + ")";
     })
-    .style('font-size','1em')
-    .style('fill','#6c6c6c')
+    .style('font-size', '1em')
+    .style('fill', '#6c6c6c')
 
 //
 d3.select(window)
     .on('resize', function () {
-         width = document.getElementById('skillchart').offsetWidth;
-         xscale = d3.scale.linear()
+        width = document.getElementById('skillchart').offsetWidth;
+        xscale = d3.scale.linear()
             .domain([0, 10])
             .range([0, width * 0.55]);
         bars.style("width", function (d) {
             return xscale(d.profency) + 'px';
         })
         greybars.style("width", function (d) {
-            return width*0.55 + 'px';
+            return width * 0.55 + 'px';
         })
     })
